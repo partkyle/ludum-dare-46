c3c75687501468a0b94a2c53d90cd3da9bff66c1
Kyle Partridge <partkyle@gmail.com>
2020-04-18 12:36:03 -0700

    first commit with fiery death

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, vertical).normalized;
+        gameObject.transform.position += direction * Time.deltaTime * speed;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Fire"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+  
+}
